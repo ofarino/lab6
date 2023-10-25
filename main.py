768683bfd253bc3af97cfc11dc43d2868263d0cc
Olivia Farino <ofarino@icloud.com>
2023-10-25 13:12:39 -0600

    Add decode function

--- main.py
+++ main.py
@@ -6,20 +6,30 @@ def encode_password(): # Kristian LaPlante Encoder
 	for char in password:
 		encoded_char = str((int(char) + 3) % 10) #  % 10  to avoid 1212
 		encoded_password += encoded_char
-	print("Your password has been encoded!")
+	print("Your password has been encoded and stored!")
+
+
+def decode(encoded_password):  # Olivia Farino decoder
+	original = ""
+	for digit in encoded_password:
+		decoded_digit = str((int(digit) - 3) % 10)
+		original += decoded_digit
+
+	return original
+
 
 while True: # Kristian LaPlante Main 
 	print("Menu\n-------------") 
 	print("1. Encode")
 	print("2. Decode")
-	Print("3. Quit")
+	print("3. Quit")
 	option = input("Please enter an option: ")
 	
 	if option == "1":
 		encode_password()
 	elif option == "2":
-		#decode_password()
-		break # Remove break once decode_password() is added
+		original_password = decode(encoded_password)
+		print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
 	elif option == "3":
 		break
 	else:
